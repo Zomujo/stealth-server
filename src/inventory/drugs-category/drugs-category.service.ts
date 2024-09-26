f429d562dbad66eca656717a8517dbb3079e68af
Kratosgado <mbeahessilfieprince@gmail.com>
2024-09-26 13:14:18 +0000

    (feat/drugs-category): add docs to service

--- src/inventory/drugs-category/drugs-category.service.ts
+++ src/inventory/drugs-category/drugs-category.service.ts
@@ -15,6 +15,14 @@ export class DrugsCategoryService {
     this.logger = new Logger(DrugsCategoryService.name);
   }
 
+  /**
+   * Creates a new drugs category.
+   *
+   * @param createDrugsCategoryDto - The DTO containing the data for creating a drugs category.
+   * @returns A promise that resolves to the created drugs category.
+   * @throws {BadRequestException} If there is a unique constraint error.
+   * @throws {InternalServerErrorException} If there is an internal server error.
+   */
   async create(createDrugsCategoryDto: CreateDrugsCategoryDto): Promise<DrugsCategoryResponse> {
     try {
       const category = await this.drugCategoryRepo.create({
@@ -31,6 +39,13 @@ export class DrugsCategoryService {
     }
   }
 
+  /**
+   * Retrieves all drugs categories.
+   *
+   * @param limit - The maximum number of categories to retrieve.
+   * @returns A promise that resolves to an array of DrugsCategoryResponse objects.
+   * @throws InternalServerErrorException if an error occurs while retrieving the categories.
+   */
   async findAll(limit: number): Promise<DrugsCategoryResponse[]> {
     try {
       this.logger.log(limit)
@@ -44,6 +59,14 @@ export class DrugsCategoryService {
     }
   }
 
+  /**
+   * Finds a drugs category by its ID.
+   *
+   * @param id - The ID of the drugs category to find.
+   * @returns A promise that resolves to the found drugs category.
+   * @throws {NotFoundException} If the drugs category with the given ID is not found.
+   * @throws {InternalServerErrorException} If an internal server error occurs.
+   */
   async findOne(id: string): Promise<DrugsCategoryResponse> {
     try {
       const category = await this.drugCategoryRepo.findByPk(id);
@@ -59,6 +82,14 @@ export class DrugsCategoryService {
     }
   }
 
+  /**
+   * Updates a drugs category.
+   *
+   * @param id - The ID of the drugs category.
+   * @param updateDrugsCategoryDto - The DTO containing the updated drugs category data.
+   * @returns A Promise that resolves to the updated drugs category.
+   * @throws InternalServerErrorException if an error occurs during the update process.
+   */
   async update(id: string, updateDrugsCategoryDto: UpdateDrugsCategoryDto) {
     try {
       const category = await this.drugCategoryRepo.upsert({ id: id, ...updateDrugsCategoryDto })
@@ -69,6 +100,13 @@ export class DrugsCategoryService {
     }
   }
 
+  /**
+   * Removes a drug category by its ID.
+   *
+   * @param id - The ID of the drug category to remove.
+   * @returns A promise that resolves to the result of the removal operation.
+   * @throws {InternalServerErrorException} If an error occurs during the removal operation.
+   */
   async remove(id: number) {
     try {
       return await this.drugCategoryRepo.destroy({ where: { id: id } })
