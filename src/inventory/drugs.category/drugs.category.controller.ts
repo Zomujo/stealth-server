3d164595320560a29970903c5c4c89d4be34ff8f
Kratosgado <mbeahessilfieprince@gmail.com>
2024-09-26 04:24:35 +0000

    add default response decorators to reduce boilerplates

--- src/inventory/drugs.category/drugs.category.controller.ts
+++ src/inventory/drugs.category/drugs.category.controller.ts
@@ -9,18 +9,33 @@ import {
 } from '@nestjs/common';
 import { DrugsCategoryService } from './drugs.category.service';
 import { CreateDrugsCategoryDto, UpdateDrugsCategoryDto } from './dto';
-import { ApiCreatedSuccessResponse } from 'src/shared/docs/decorators/response.decorators';
+import { ApiCreatedSuccessResponse, ApiSuccessResponse } from 'src/shared/docs/decorators/response.decorators';
 import { ApiBadRequestResponse, ApiInternalServerErrorResponse, ApiTags } from '@nestjs/swagger';
 import { ApiErrorResponse } from 'src/utils/responses/error.response';
+import { ApiSuccessResponseDto } from 'src/utils/responses/success.response';
+import { CustomApiResponse } from 'src/shared/docs/decorators/default.response.decorators';
+import { DrugsCategory } from './models/drugs.category.model';
 
 @ApiTags("Drug Category")
 @Controller('drugs/category')
 export class DrugsCategoryController {
   constructor(private readonly drugsCategoryService: DrugsCategoryService) { }
 
-  @ApiCreatedSuccessResponse({
+  @CustomApiResponse(["created", "forbidden", "unauthorized"], {type: DrugsCategory, message: "Drug category created successfully"})
+  @Post()
+  async create(@Body() createDrugsCategoryDto: CreateDrugsCategoryDto) {
+    return await this.drugsCategoryService.create(createDrugsCategoryDto);
+  }
+
+  @CustomApiResponse(["accepted", "forbidden", "unauthorized"], { type: DrugsCategory, isArray: true, message: "Drug categories retrieved successfully" })
+  @Get()
+  async findAll() {
+    return await this.drugsCategoryService.findAll();
+  }
+
+  @ApiSuccessResponse({
     type: CreateDrugsCategoryDto,
-    description: "Drug category added successfully"
+    description: "Drug category retrieved successfully",
   })
   @ApiBadRequestResponse({
     type: ApiErrorResponse,
@@ -30,21 +45,23 @@ export class DrugsCategoryController {
     type: ApiErrorResponse,
     description: 'An unexpected error occured',
   })
-  @Post()
-  async create(@Body() createDrugsCategoryDto: CreateDrugsCategoryDto) {
-    return  await this.drugsCategoryService.create(createDrugsCategoryDto);
-  }
-
-  @Get()
-  async findAll() {
-    return await this.drugsCategoryService.findAll();
-  }
-
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.drugsCategoryService.findOne(+id);
   }
 
+  @ApiSuccessResponse({
+    type: CreateDrugsCategoryDto,
+    description: "Drug category updated successfully",
+  })
+  @ApiBadRequestResponse({
+    type: ApiErrorResponse,
+    description: "validation error occured"
+  })
+  @ApiInternalServerErrorResponse({
+    type: ApiErrorResponse,
+    description: 'An unexpected error occured',
+  })
   @Patch(':id')
   update(
     @Param('id') id: string,
@@ -53,6 +70,18 @@ export class DrugsCategoryController {
     return this.drugsCategoryService.update(+id, updateDrugsCategoryDto);
   }
 
+  @ApiSuccessResponse({
+    type: CreateDrugsCategoryDto,
+    description: "Drug category deleted successfully",
+  })
+  @ApiBadRequestResponse({
+    type: ApiErrorResponse,
+    description: "validation error occured"
+  })
+  @ApiInternalServerErrorResponse({
+    type: ApiErrorResponse,
+    description: 'An unexpected error occured',
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.drugsCategoryService.remove(+id);
