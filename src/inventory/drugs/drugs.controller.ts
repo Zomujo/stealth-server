5b378e91b6bac41612ba6dcd8f16529d157a902d
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-10-18 12:25:51 +0000

    Update src/inventory/drugs/drugs.controller.ts
    Co-authored-by: Ebenezer Domey <73116752+EbenDomey@users.noreply.github.com>

--- src/inventory/drugs/drugs.controller.ts
+++ src/inventory/drugs/drugs.controller.ts
@@ -145,8 +145,7 @@ export class DrugsController {
     }
   }
 
-  @CustomApiResponse(['success', 'authorize'], {
-    type: String,
+  @CustomApiResponse(['successNull', 'authorize'], {
     message: 'Drug prices adjusted successfully',
   })
   @Roles(
