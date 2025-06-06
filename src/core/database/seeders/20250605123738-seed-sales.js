30d079f9df668f8bea398ec73aedf7c13321fdc9
Kratosgado <mbeahessilfieprince@gmail.com>
2025-06-06 10:04:42 +0000

    add: seeds for sales

--- src/core/database/seeders/20250605123738-seed-sales.js
+++ src/core/database/seeders/20250605123738-seed-sales.js
@@ -2,11 +2,13 @@
 
 const faker = require('@faker-js/faker').faker;
 const { v4: uuidv4 } = require('uuid');
+const { createdAt } = require('../migration-base');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.startTransaction();
+    // const transaction = new Sequelize.Transaction();
+    // await queryInterface.startTransaction(transaction);
 
     const items = await queryInterface.sequelize.query(
       'SELECT i.id,i.facility_id,i.selling_price, b.id batchId, b.quantity FROM items i JOIN batches b ON b.item_id = i.id;',
@@ -16,21 +18,25 @@ module.exports = {
     const sales = [];
     const saleItems = [];
 
-    for (s = 0; s < 50; s++) {
+    for (let s = 0; s < 50; s++) {
       const saleId = uuidv4();
-      const lastMonth = new Date.UTC(2025, new Date().getMonth(), 1);
+      const lastMonth = Date.UTC(2025, new Date().getMonth(), 1);
+      const createdAt = faker.date.between({ from: lastMonth, to: new Date() });
+      const updatedAt = faker.date.between({ from: lastMonth, to: new Date() });
       let total = 0;
 
-      for (si = 0; s < 5; s++) {
+      for (let si = 0; si < 5; si++) {
         const item = items[faker.number.int(items.length - 1)];
         total += item.selling_price;
         saleItems.push({
           id: uuidv4(),
           sale_id: saleId,
           item_id: item.id,
-          batch_id: item.batchId,
+          batch_id: item.batchid,
           quantity: faker.number.int(item.quantity % 10),
           facility_id: item.facility_id,
+          created_at: createdAt,
+          updated_at: updatedAt,
         });
       }
 
@@ -40,20 +46,23 @@ module.exports = {
         sale_number: `S-${faker.number.int({ min: 1000, max: 9999 })}`,
         notes: faker.lorem.sentences(2),
         total: total,
+        sub_total: total,
         status: 'PAID',
-        created_at: faker.date.between({ from: lastMonth, to: new Date() }),
+        created_at: createdAt,
+        updated_at: updatedAt,
       });
     }
 
-    await queryInterface.bulkInsert('sales', sales, {});
-    await queryInterface.bulkInsert('sale_items', saleItems, {});
-    await queryInterface.commitTransaction();
+    await queryInterface.bulkInsert('sales', sales);
+    await queryInterface.bulkInsert('sale_items', saleItems);
+    // await transaction.commit();
+    // await queryInterface.commitTransaction(transaction);
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.startTransaction();
+    // const transaction = new Sequelize.Transaction();
     await queryInterface.bulkDelete('sale_items');
     await queryInterface.bulkDelete('sales');
-    await queryInterface.commitTransaction();
+    // await transaction.commit();
   },
 };
