11664d0416d980ec191e65c118e47543090ed21e
EbenDomey <domeyeben4@gmail.com>
2025-06-14 18:39:13 +0000

    feat(sms): almost there

--- src/notification/sms/sms.service.ts
+++ src/notification/sms/sms.service.ts
@@ -2,57 +2,57 @@ import { Injectable } from '@nestjs/common';
 import { CreateSmsDto } from './dto';
 import { ConfigService } from '@nestjs/config';
 import { TwilioService } from 'nestjs-twilio';
+import { MessageInstance } from 'nestjs-twilio/node_modules/twilio/lib/rest/api/v2010/account/message';
 import AfricasTalking from 'africastalking';
 import { SMSMessageData } from './types';
-import { MessageInstance } from 'twilio/lib/rest/api/v2010/account/message';
 
 @Injectable()
 export class SmsService {
-  constructor(
-    private configService: ConfigService,
-    private twilioService: TwilioService,
-  ) {}
-  create(_createSmDto: CreateSmsDto) {
-    return 'This action adds a new sm';
-  }
+	constructor(
+		private configService: ConfigService,
+		private twilioService: TwilioService,
+	) { }
+	create(_createSmDto: CreateSmsDto) {
+		return 'This action adds a new sm';
+	}
 
-  private get africastalkingClient() {
-    return AfricasTalking({
-      apiKey: this.configService.get<string>('ATSK_API_KEY'),
-      username: this.configService.get<string>('ATSK_USERNAME'),
-    });
-  }
+	private get africastalkingClient() {
+		return AfricasTalking({
+			apiKey: this.configService.get<string>('ATSK_API_KEY'),
+			username: this.configService.get<string>('ATSK_USERNAME'),
+		});
+	}
 
-  async sendSms(dto: CreateSmsDto) {
-    return await this.sendWithAfricasTalking({
-      body: dto.body,
-      to: dto.to,
-    });
-    // return this.twilioService.client.messages.create();
-  }
+	async sendSms(dto: CreateSmsDto) {
+		return await this.sendWithAfricasTalking({
+			body: dto.body,
+			to: dto.to,
+		});
+		// return this.twilioService.client.messages.create();
+	}
 
-  async sendWithAfricasTalking(dto: CreateSmsDto): Promise<SMSMessageData> {
-    const from = this.configService.get<string>('ATSK_SHORT_CODE');
-    return this.africastalkingClient.SMS.send({
-      to: dto.to,
-      from: from,
-      message: dto.body,
-    });
-  }
-  async sendWithTwilio(dto: CreateSmsDto): Promise<MessageInstance> {
-    const from = this.configService.get<string>('TWILIO_PHONE_NUMBER');
-    return this.twilioService.client.messages.create({
-      body: dto.body,
-      from: from,
-      to: dto.to,
-    });
-  }
+	async sendWithAfricasTalking(dto: CreateSmsDto): Promise<SMSMessageData> {
+		const from = this.configService.get<string>('ATSK_SHORT_CODE');
+		return this.africastalkingClient.SMS.send({
+			to: dto.to,
+			from: from,
+			message: dto.body,
+		});
+	}
+	async sendWithTwilio(dto: CreateSmsDto): Promise<MessageInstance> {
+		const from = this.configService.get<string>('TWILIO_PHONE_NUMBER');
+		return this.twilioService.client.messages.create({
+			body: dto.body,
+			from: from,
+			to: dto.to,
+		});
+	}
 
-  findAll() {
-    return `This action returns all sms`;
-  }
+	findAll() {
+		return `This action returns all sms`;
+	}
 
-  findOne(id: number) {
-    return `This action returns a #${id} sm`;
-  }
+	findOne(id: number) {
+		return `This action returns a #${id} sm`;
+	}
 }
