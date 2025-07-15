3ef36413e3d294363627ea2da75b97a9e7c332cb
EbenDomey <domeyeben4@gmail.com>
2025-07-15 22:57:54 +0000

    fixed(username)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -174,7 +174,7 @@ export class AuthService {
       where: {
         [Op.or]: [
           { email: dto.accountIdentifier },
-          { username: dto.accountIdentifier },
+          { username: { [Op.iLike]: dto.accountIdentifier } },
         ],
       },
     });
